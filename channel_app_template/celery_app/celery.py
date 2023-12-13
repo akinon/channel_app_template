a366da08c2f156c8d7960c5a0b28c405acaa9dbb
ziyacivan <yusufziya.civan@akinon.com>
2023-12-13 16:49:17 +0300

    OMNI-2714: Add exception handler to the Sentry before event sending

--- channel_app_template/celery_app/celery.py
+++ channel_app_template/celery_app/celery.py
@@ -1,3 +1,5 @@
+import hashlib
+import time
 from celery import Celery
 from celery.signals import celeryd_init
 
@@ -9,7 +11,6 @@ from channel_app_template.celery_app.celery_schedule_conf import \
     CELERYBEAT_SCHEDULE
 
 app = Celery('celery_app')
-
 
 ## Broker settings.
 app.config_from_object('channel_app_template.celery_app.celeryconfig')
@@ -23,9 +24,25 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = CELERYBEAT_SCHEDULE
 
 
+def report_to_sentry(exception):
+    exception_hash = hashlib.md5(str(exception).encode('utf-8')).hexdigest()
+    last_reported_time = settings.redis_instance.get(exception_hash)
+
+    if not last_reported_time or time.time() - last_reported_time > 60 * 15:
+        sentry_sdk.capture_exception(exception)
+        # Set to the redis for 15 minutes
+        settings.redis_instance.set(exception_hash, time.time(), ex=60 * 15)
+
+
+def strip_sensitive_data(event, hit):
+    if hit.get('exception'):
+        return report_to_sentry(hit['exception'])
+
+
 @celeryd_init.connect()
 def configure_sentry(**kwargs):
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
-        integrations=[CeleryIntegration()]
+        integrations=[CeleryIntegration()],
+        before_send=strip_sensitive_data
     )
--- channel_app_template/settings.py
+++ channel_app_template/settings.py
@@ -1,2 +1,7 @@
+import redis
 from channel_app_template import *
 from channel_app.core.settings import *
+
+redis_instance = redis.Redis(host=BROKER_HOST, port=BROKER_PORT, db=BROKER_DATABASE_INDEX)
+redis_instance.flushall()
+
